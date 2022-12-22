bb0648ff5961e961b540714e4cf576eb461b2be6
박상준 <cosmoquester@gmail.com>
2022-12-22 15:01:21 +0900

    build: Set build informations

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="python3-template",
+    name="torch-lightning-template",
     version="0.0.1",
-    description="This repository is template for my python project.",
-    python_requires='>=3.7',
+    description="This repository is template for my pytorch lightning project.",
+    python_requires=">=3.7",
     install_requires=[],
-    url="https://github.com/cosmoquester/python3-template.git",
+    url="https://github.com/cosmoquester/torch-lightning-template.git",
     author="Park Sangjun",
     packages=find_packages(exclude=["tests"]),
 )
